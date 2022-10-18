95f220126d918c3e73e5a20e77bde72d45036878
Xer0n3 - RussC <developing821@outlook.com>
2022-10-18 15:57:20 +0100

    Hues, refactored
    implemented and refactored hues from Shader object RadialGradient

---
+++ app/src/main/java/com/developing821/i/RadialGradient.java
@@ -0,0 +1,31 @@
+package com.developing821.i;
+
+import static android.graphics.Shader.TileMode.DECAL;
+
+import android.graphics.Shader;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.S)
+public class RadialGradient extends Shader {
+
+    TileMode tileMode = DECAL;
+    float[] stops = null;
+
+    /**
+     * Simply set the colour gradient based on the periferals
+     * @param startX
+     * @param startY
+     * @param startRadius -
+     * @param endX
+     * @param endY
+     * @param endRadius
+     * @param colors - int: The sRGB colors to be distributed between the center and edge of the circle This value cannot be null.
+     * @param stops - float: May be null. Valid values are between 0.0f and 1.0f. The relative position of each corresponding color in the colors array. If null, colors are distributed evenly between the center and edge of the circle.
+     * @param tileMode - Shader.TileMode: The Shader tiling mode This value cannot be null. - https://developer.android.com/reference/android/graphics/Shader.TileMode - DECAL
+     */
+    public RadialGradient(float startX, float startY, float startRadius, float endX, float endY, float endRadius, long[] colors, float[] stops, TileMode tileMode) {
+        return;
+    }
+
+}
